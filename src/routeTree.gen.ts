f8694d5b1a430161a280d25fdf807091864f6d78
Kauê Pastrello <120512459+kauefsantos@users.noreply.github.com>
2026-09-07 16:31:29 -0300

    Remove superseded experimental corners route from route tree

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,7 +10,6 @@
 
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
-import { Route as ExperimentalCornersRouteImport } from './routes/experimental.corners'
 import { Route as RunRunIdOportunidadesRouteImport } from './routes/run.$runId.oportunidades'
 import { Route as RunRunIdProcessamentoRouteImport } from './routes/run.$runId.processamento'
 import { Route as RunRunIdResultadoRouteImport } from './routes/run.$runId.resultado'
@@ -18,11 +17,6 @@ import { Route as RunRunIdResultadoRouteImport } from './routes/run.$runId.resul
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
-  getParentRoute: () => rootRouteImport,
-} as any)
-const ExperimentalCornersRoute = ExperimentalCornersRouteImport.update({
-  id: '/experimental/corners',
-  path: '/experimental/corners',
   getParentRoute: () => rootRouteImport,
 } as any)
 const RunRunIdOportunidadesRoute = RunRunIdOportunidadesRouteImport.update({
@@ -43,14 +37,12 @@ const RunRunIdResultadoRoute = RunRunIdResultadoRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
-  '/experimental/corners': typeof ExperimentalCornersRoute
   '/run/$runId/oportunidades': typeof RunRunIdOportunidadesRoute
   '/run/$runId/processamento': typeof RunRunIdProcessamentoRoute
   '/run/$runId/resultado': typeof RunRunIdResultadoRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
-  '/experimental/corners': typeof ExperimentalCornersRoute
   '/run/$runId/oportunidades': typeof RunRunIdOportunidadesRoute
   '/run/$runId/processamento': typeof RunRunIdProcessamentoRoute
   '/run/$runId/resultado': typeof RunRunIdResultadoRoute
@@ -58,7 +50,6 @@ export interface FileRoutesByTo {
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
-  '/experimental/corners': typeof ExperimentalCornersRoute
   '/run/$runId/oportunidades': typeof RunRunIdOportunidadesRoute
   '/run/$runId/processamento': typeof RunRunIdProcessamentoRoute
   '/run/$runId/resultado': typeof RunRunIdResultadoRoute
@@ -67,21 +58,18 @@ export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
     | '/'
-    | '/experimental/corners'
     | '/run/$runId/oportunidades'
     | '/run/$runId/processamento'
     | '/run/$runId/resultado'
   fileRoutesByTo: FileRoutesByTo
   to:
     | '/'
-    | '/experimental/corners'
     | '/run/$runId/oportunidades'
     | '/run/$runId/processamento'
     | '/run/$runId/resultado'
   id:
     | '__root__'
     | '/'
-    | '/experimental/corners'
     | '/run/$runId/oportunidades'
     | '/run/$runId/processamento'
     | '/run/$runId/resultado'
@@ -89,7 +77,6 @@ export interface FileRouteTypes {
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
-  ExperimentalCornersRoute: typeof ExperimentalCornersRoute
   RunRunIdOportunidadesRoute: typeof RunRunIdOportunidadesRoute
   RunRunIdProcessamentoRoute: typeof RunRunIdProcessamentoRoute
   RunRunIdResultadoRoute: typeof RunRunIdResultadoRoute
@@ -102,13 +89,6 @@ declare module '@tanstack/react-router' {
       path: '/'
       fullPath: '/'
       preLoaderRoute: typeof IndexRouteImport
-      parentRoute: typeof rootRouteImport
-    }
-    '/experimental/corners': {
-      id: '/experimental/corners'
-      path: '/experimental/corners'
-      fullPath: '/experimental/corners'
-      preLoaderRoute: typeof ExperimentalCornersRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/run/$runId/oportunidades': {
@@ -137,7 +117,6 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
-  ExperimentalCornersRoute: ExperimentalCornersRoute,
   RunRunIdOportunidadesRoute: RunRunIdOportunidadesRoute,
   RunRunIdProcessamentoRoute: RunRunIdProcessamentoRoute,
   RunRunIdResultadoRoute: RunRunIdResultadoRoute,
